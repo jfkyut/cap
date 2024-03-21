946630c1bb527f9564fdd6c148c3743c28d56edf
jfkyut <jfarnosa.github@gmail.com>
2024-03-21 17:40:26 +0800

    updated travel view

--- client/src/views/travel/travel/TravelView.vue
+++ client/src/views/travel/travel/TravelView.vue
@@ -3,6 +3,7 @@ import { useTravelStore } from '@/stores/travel';
 import { storeToRefs } from 'pinia';
 import { computed, onMounted, watch } from 'vue';
 import { useRoute } from 'vue-router';
+import PrimaryButton from '@/components/buttons/PrimaryButton.vue';
 
 const { travel, activeTravelId } = storeToRefs(useTravelStore());
 const route = useRoute();
@@ -19,22 +20,36 @@ watch(travelId, (travelId) => {
 </script>
 
 <template>
-  <div class="space-y-6">
+
+  <div class="fixed z-10 bottom-0">
+    <PrimaryButton>
+      <i class="fa fa-edit"></i>
+    </PrimaryButton>
+  </div>
+
+  <div class="space-y-6 max-w-4xl mx-auto my-6">
     <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
       <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+        <thead class="text-xs text-gray-700 uppercase bg-blue-100 dark:bg-gray-700 dark:text-gray-400">
+          <tr class="border-b dark:border-gray-600">
+            <th scope="col" colspan="5" class="px-6 py-3 truncate lg:text-center">
+              <span class="text-lg dark:text-gray-300">
+                {{ travel?.title }}
+              </span>
+            </th>
+          </tr>
           <tr>
-              <th scope="col" class="px-6 py-3">
-                  Category
-              </th>
-              <th scope="col" class="px-6 py-3">
-                  Details
-              </th>
+            <th scope="col" class="px-6 py-3 truncate">
+                Type
+            </th>
+            <th scope="col" class="px-6 py-3 truncate">
+                Details
+            </th>
           </tr>
         </thead>
         <tbody>
           <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white w-[50%] truncate">
               Destination
             </th>
             <td class="px-6 py-4">
@@ -42,62 +57,62 @@ watch(travelId, (travelId) => {
             </td>
           </tr>
           <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white w-[50%] truncate">
               Arrival Date
             </th>
-            <td class="px-6 py-4">
+            <td class="px-6 py-4 w-[50%] truncate">
               {{ travel?.plan.arrival }}
             </td>
           </tr>
           <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white w-[50%] truncate">
               Departure Date
             </th>
-            <td class="px-6 py-4">
+            <td class="px-6 py-4 w-[50%] truncate">
               {{ travel?.plan.departure }}
             </td>
           </tr>
           <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white w-[50%] truncate">
               Duration
             </th>
-            <td class="px-6 py-4">
+            <td class="px-6 py-4 w-[50%] truncate">
               {{ travel?.plan.duration }}
             </td>
           </tr>
           
           <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white w-[50%] truncate">
               Activities
             </th>
-            <td class="px-6 py-4">
+            <td class="px-6 py-4 w-[50%] truncate">
               {{ travel?.plan.activities?.length > 0 && travel.plan.activities.join(', ') }}
             </td>
           </tr>
 
           <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white w-[50%] truncate">
               Accomodation type preference
             </th>
-            <td class="px-6 py-4">
+            <td class="px-6 py-4 w-[50%] truncate">
               {{ travel?.plan.accommodation_suggestion?.length > 0 && travel.plan.accommodation_suggestion.join(', ') }}
             </td>
           </tr>
 
           <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white w-[50%] truncate">
               Inclusion
             </th>
-            <td class="px-6 py-4">
+            <td class="px-6 py-4 w-[50%] truncate">
               {{ travel?.plan.inclusion?.length > 0 && travel.plan.inclusion.join(', ') }}
             </td>
           </tr>
 
           <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white w-[50%] truncate">
               Exclusion
             </th>
-            <td class="px-6 py-4">
+            <td class="px-6 py-4 w-[50%] truncate">
               {{ travel?.plan.exclusion?.length > 0 && travel.plan.exclusion.join(', ') }}
             </td>
           </tr>
@@ -105,48 +120,53 @@ watch(travelId, (travelId) => {
       </table>
     </div>
 
-
-
     <div class="relative overflow-x-auto shadow-md sm:rounded-lg" v-for="(day, index) in travel?.plan.daily" :key="index">
       <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-            <tr>
-              <th scope="col" class="px-6 py-3">
-                Activity
-              </th>
-              <th scope="col" class="px-6 py-3">
-                Start time
-              </th>
-              <th scope="col" class="px-6 py-3">
-                End time
-              </th>
-              <th scope="col" class="px-6 py-3">
-                Duration
-              </th>
-              <th scope="col" class="px-6 py-3">
-                Estimated budget
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600" v-for="(activity, index) in day.activities" :key="index">
-              <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                {{ activity.name }}
-              </th>
-              <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                {{ activity.start_time }}
-              </th>
-              <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                {{ activity.end_time }}
-              </th>
-              <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                {{ activity.duration }}
-              </th>
-              <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                {{ activity.estimated_budget }}
-              </th>
-            </tr>
-          </tbody>
+        <thead class="text-xs text-gray-700 uppercase bg-blue-100 dark:bg-gray-700 dark:text-gray-400">
+          <tr class="border-b dark:border-gray-600">
+            <th scope="col" colspan="5" class="px-6 py-3 truncate lg:text-center">
+              <span class="text-lg dark:text-gray-300">
+                Day {{ day.day }}: {{ day.date }}
+              </span>
+            </th>
+          </tr>
+          <tr>
+            <th scope="col" class="px-6 py-3 truncate">
+              Activity
+            </th>
+            <th scope="col" class="px-6 py-3 truncate">
+              Start time
+            </th>
+            <th scope="col" class="px-6 py-3 truncate">
+              End time
+            </th>
+            <th scope="col" class="px-6 py-3 truncate">
+              Duration
+            </th>
+            <th scope="col" class="px-6 py-3 truncate">
+              Estimated budget
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600" v-for="(activity, index) in day.activities" :key="index">
+            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white w-[40%] truncate">
+              {{ activity.name }}
+            </th>
+            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white w-[15%] truncate">
+              {{ activity.start_time }}
+            </th>
+            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white w-[15%] truncate">
+              {{ activity.end_time }}
+            </th>
+            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white w-[15%] truncate">
+              {{ activity.duration }}
+            </th>
+            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white w-[15%] truncate">
+              {{ activity.estimated_budget }}
+            </th>
+          </tr>
+        </tbody>
       </table>
     </div>
 

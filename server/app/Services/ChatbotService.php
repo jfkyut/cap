f05b09861acf3fc3633eda8b929d86df73198bcc
jfkyut <jfarnosa.github@gmail.com>
2024-03-26 11:27:04 +0800

    tweak prompt

--- server/app/Services/ChatbotService.php
+++ server/app/Services/ChatbotService.php
@@ -22,7 +22,7 @@ class ChatbotService
         $this->travelGeneratorInitialMessage = [
             [
                 'role' => 'system',
-                'content' => 'You are Travel Itinerary Generator. Generate a travel itinerary base on the user input. Detail every activity from date of arrival, checkin (always include), transportation (Please always include this when there is more than 1 barangay to have smooth transition of activities), dining, checkout (always include), suggest for both inclusions and exclusions (this can be anything, you can be creative of at least putting something in both fields), suggest accommodation base on user preference, departure and include it in daily activities if necessary. Reply in a json format. format: ' . Storage::get("travel.json"),
+                'content' => 'You are Travel Itinerary Generator. Generate a travel itinerary base on the user input. Detail every activity from date of arrival, checkin (always include), transportation between barangays (Please fucking always include this when there is more than 1 barangay to have smooth transition of activities between barangays and not produce a not realistic situation. I keep tweaking the prompt, please listen.), dining, checkout (always include), suggest for both inclusions and exclusions (this can be anything, you can be creative of at least putting something in both fields), suggest accommodation base on user preference, departure and include it in daily activities if necessary. Reply in a json format. format: ' . Storage::get("travel.json"),
             ]
         ];
     }
